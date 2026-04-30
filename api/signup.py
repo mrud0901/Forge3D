6ae3691b48de2ae3c60ba5b9806a2c5e486de199
mrudula <mrudmakar@gmail.com>
2026-04-30 07:07:42 +0530

    fixing the code

--- api/signup.py
+++ api/signup.py
@@ -3,17 +3,27 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers.get('Content-Length', 0))
-        body = self.rfile.read(content_length)
+        try:
+            content_length = int(self.headers.get('Content-Length', 0))
+            body = self.rfile.read(content_length)
 
-        data = json.loads(body)
+            data = json.loads(body)
 
-        response = {
-            "message": "Signup successful",
-            "user": data.get("email")
-        }
+            email = data.get("email")
+            password = data.get("password")
 
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        self.wfile.write(json.dumps(response).encode())
+            response = {
+                "message": "Signup successful",
+                "email": email
+            }
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode())
+
+        except Exception as e:
+            self.send_response(500)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"error": str(e)}).encode())
